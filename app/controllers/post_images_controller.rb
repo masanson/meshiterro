35113c1aa2f8ec5a9572b6d78260db61b0c58ace
masanson <138214683+masanson@users.noreply.github.com>
2023-07-21 11:53:25 +0000

    [Update] meshiterro_chapter11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,14 +4,14 @@ class PostImagesController < ApplicationController
   end
 
   def create
-    @post_image = PostImage.new
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path 
   end
 
   def index
-    @post_image =PostImage.all
+    @post_images = PostImage.all
   end
 
   def show
